9c4c36798629148b219e54fd4ebb331512a631a7
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-09-13 10:22:22 -0300

    Aula 02

---
+++ src/components/empty-goals.tsx
@@ -0,0 +1,26 @@
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+
+import letsStart from '../../src/assets/lets-start.svg'
+import logo from '../../src/assets/logo-in-orbit.svg'
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
+      </p>
+
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
